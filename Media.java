dce30d0c30ca53419630e5b3340fe90bbbed4fb5
JordyBU <102196693+JordyBU@users.noreply.github.com>
2022-04-25 20:43:14 -0600

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,14 @@
+public class Media {
+public static void main(String[]args){
+
+
+float n=media(12, 23);
+    System.out.println("la media es de: "+n);
+        }
+
+static  float media (float n1,float n2){
+    
+    return (n1+n2)/2;
+}}
+  
+
